eacf04e30b76ea0b263a6f9013843ab66956a562
Johanna Kruusement <kruusement.johanna@gmail.com>
2024-12-26 14:53:04 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is a comment to my code
+
+// rakendus küsib kasutajal sisestada tema nime
+// rakendus tervitab kasutaat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõna
+string userName;
+
+userName = Console.ReadLine(); //Input
+
+Console.WriteLine($"Hello, {userName}!"); //Output
+
+
+
+
+
+
